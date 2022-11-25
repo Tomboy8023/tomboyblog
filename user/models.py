01f032a97c3e0ae763b22766f14d89347cb6eb27
Tomboy <Tomboy1996@163.com>
2022-11-25 11:06:17 +0800

    第一次提交：项目搭建 + 项目配置

---
+++ user/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# class Users(models.Model):
+#     id = models.BigAutoField(primary_key=True, verbose_name="自增ID")
+#     uname = models.CharField(max_length=20, verbose_name="用户名")
+#     userid = models.IntegerField(verbose_name="用户ID")
+#
